f600dcbac286b0429b902de0c58bb179b4b8bf2b
sneezeontoast <gospel.enquires@gmail.com>
2025-08-15 10:08:02 +0100

    Working me thinks

--- steer.py
+++ steer.py
@@ -3,19 +3,18 @@ from gpiozero import DigitalOutputDevice
 import time
 
 # Change colour ones
-actuator_out_pin_1 = DigitalOutputDevice(18) # neg
-actuator_out_pin_2 = DigitalOutputDevice(23) # pos
+actuator_in_pin_1 = DigitalOutputDevice(18)  # neg
+actuator_in_pin_2 = DigitalOutputDevice(23)  # pos
 
 # Same colour ones
-actuator_in_pin_1 = DigitalOutputDevice(22) # neg
-actuator_in_pin_2 = DigitalOutputDevice(17) # pos
-
-
-
+actuator_out_pin_1 = DigitalOutputDevice(22)  # neg
+actuator_out_pin_2 = DigitalOutputDevice(17)  # pos
 
 
 # loop through 50 times, on/off for 1 second
 def right():
+    print("Moving right")
+
     actuator_out_pin_1.on()
     actuator_out_pin_2.on()
     time.sleep(4)
@@ -24,11 +23,13 @@ def right():
 
 
 def left():
+    print("Moving left")
     actuator_in_pin_1.on()
     actuator_in_pin_2.on()
     time.sleep(4)
     actuator_in_pin_1.off()
     actuator_in_pin_2.off()
+
 
 right()
 left()
