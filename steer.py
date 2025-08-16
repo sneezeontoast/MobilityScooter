17494483e1254b152d3eff748523c0b0238bacb9
sneezeontoast <gospel.enquires@gmail.com>
2025-08-16 18:53:02 +0100

    Actuator works change timings

--- steer.py
+++ steer.py
@@ -3,43 +3,33 @@ from gpiozero import DigitalOutputDevice
 import time
 
 # Change colour ones
-actuator_in_pin_1 = DigitalOutputDevice(21)  # neg
-actuator_in_pin_2 = DigitalOutputDevice(17)  # pos
+actuator_in_pin_1 = DigitalOutputDevice(18)  # neg
+actuator_in_pin_2 = DigitalOutputDevice(26)  # pos
 
 # Same colour ones
-actuator_out_pin_1 = DigitalOutputDevice(16)  # neg
-actuator_out_pin_2 = DigitalOutputDevice(18)  # pos
+actuator_out_pin_1 = DigitalOutputDevice(21)  # neg
+actuator_out_pin_2 = DigitalOutputDevice(17)  # pos
 
-def reset_relays():
-    actuator_out_pin_1.off()
-    actuator_out_pin_2.off()
-    actuator_in_pin_1.off()
-    actuator_in_pin_2.off()
 
 # loop through 50 times, on/off for 1 second
 def right():
     print("Moving right")
-    reset_relays()
+
     actuator_out_pin_1.on()
     actuator_out_pin_2.on()
     time.sleep(2)
     actuator_out_pin_1.off()
     actuator_out_pin_2.off()
-    time.sleep(0.01)
 
 
 def left():
     print("Moving left yellow wire not reciving")
-    reset_relays
     actuator_in_pin_1.on()
     actuator_in_pin_2.on()
     time.sleep(2)
     actuator_in_pin_1.off()
     actuator_in_pin_2.off()
-    time.sleep(0.01)
 
-reset_relays()
-right()
-print("Switching to left")
-time.sleep(2)
+
+# right()
 left()
